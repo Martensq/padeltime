76c1ea93a343d2187cefb296e24dad49d536d144
Quentin <quentin.martens@orange.fr>
2024-05-29 13:06:21 +0200

    Espace visiteur / Création logique formulaire contact + envoi mail partie 2 et fin

--- src/Controller/Visitor/Contact/ContactController.php
+++ src/Controller/Visitor/Contact/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Visitor\Contact;
 use DateTimeImmutable;
 use App\Entity\Contact;
 use App\Form\ContactFormType;
+use App\Service\SendEmailService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'visitor_contact_index', methods: ['GET', 'POST'])]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, SendEmailService $sendEmailService): Response
     {
         // 1- Créons l'instance du contact qui doit être ajouté en bdd
         $contact = new Contact();
@@ -39,8 +40,24 @@ class ContactController extends AbstractController
             // Exécuter la requête
             $entityManager->flush();
 
+            // Envoyer le mail à l'admin pour prévenir du nouveau message
+            $sendEmailService->send([
+                "sender_email" => "quentin.martens@orange.fr",
+                "sender_name" => "Padel Time",
+                "recipient_email" => "quentin.martens@orange.fr",
+                "subject" => "Nouveau message reçu",
+                "html_template" => "emails/contact.html.twig",
+                "context" => [
+                    "contact_first_name"    => $contact->getFirstName(),
+                    "contact_last_name"     => $contact->getLastName(),
+                    "contact_email"         => $contact->getEmail(),
+                    "contact_phone"         => $contact->getPhone(),
+                    "contact_message"       => $contact->getMessage(),
+                ]
+            ]);
+
             // Générer un message flash de succès
-            $this->addFlash("success", "Le contact a été ajouté avec succès");
+            $this->addFlash("success", "Le message a bien été envoyé. Nous vous répondons rapidement.");
 
             // Effectuer une redirection vers la page d'accueil afin de consulter le nouveau contact ajouté dans la liste
             // Puis arrêter l'exécution du script
